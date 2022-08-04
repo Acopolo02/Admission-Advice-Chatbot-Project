2f51f1f16796375a33d6c84188edcbe16cf12042
Acopolo02 <oluwaseunadeoye01@gmail.com>
2022-08-04 17:11:40 +0100

    SQLite Database - structured data

---
+++ db/db_func.py
@@ -0,0 +1,15 @@
+modules =[{
+    "Levels":"First Year",
+    "Modules":[
+        {"Name": "Genetic Analysis", "Description":"You'll take a  problem-based approach to understanding modern molecular"}
+    ]
+},
+    {
+        "Levels":"Second year",
+        "Modules":[
+            {"Name":"", "Description":""}
+        ]
+    }
+
+
+]
